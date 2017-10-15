d85c8c3662eb55fee28ee209e21a084f567104c4
Bayan Berri <bberri@stuy.edu>
2017-10-14 23:39:27 -0400

    not sure what was wrong b4 but it works now

--- db_builder.py
+++ db_builder.py
@@ -15,34 +15,24 @@ crses= open("courses.csv")
 courses= csv.DictReader(crses)
 
 #==========================================================
-'''for each in peeps:
-   # print each #+":" each['name']+','+each['age']+','+each['id']
-    print each['name']
-    print each['age']
-    print each['id']
-'''
+              
+command= "CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER)"
+c.execute(command)
 
-command = "CREATE TABLE people (name TEXT, age INTEGER, id INTEGER)" #put SQL statement in this string
-c.execute(command)    #run SQL statement
-print "table people created\n\n"
+command= "CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)"
+c.execute(command)
 
-def populate(d, one, two,three,tbl):
-    for i in d:
-        print i
-        c.execute("INSERT INTO "+tbl+" VALUES("+i[one]+","+ i[two]+","+i[three]+")")
-        print "row inserted"
-    print "inserted all things to tbl"
+for each in peeps:
+    print each
+    add= "INSERT INTO peeps VALUES ('" + each['name'] + "'," + each['age'] + "," + each['id'] + ")"
+    c.execute(add)
+
+for each in courses:
+    print each
+    add= "INSERT INTO courses VALUES ('" + each['code'] + "'," + each['mark'] + "," + each['id'] + ")"
+    c.execute(add)
+
     
-populate(peeps, 'name','age','id','people')
-
-'''comm = "CREATE TABLE classes(code TEXT, mark INTEGER, id INTEGER)"
-c.execute (comm)
-
-for i in courses:
-    c.execute("INSERT INTO classes VALUES("+i['code']+","+ i['mark']+","+i['id']+")")
-
-'''
-
 #==========================================================
 
 db.commit() #save changes
